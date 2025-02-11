589d11344b080b3eca5e30150de40d8ae7e47557
DomJ84 <Dominic-Johnson@Hotmail.co.uk>
2025-02-11 14:07:45 +0000

    Add files via upload

---
+++ Modify Existing File.py
@@ -0,0 +1,15 @@
+
+x=0
+
+while x<5:
+    file = open("newfile.txt", "r")
+    z = (file.read())
+    file.close ()
+    file = open("newfile.txt", "a")
+    file.write(z)
+    file.write(str(x))
+    file.write("\n")
+    file.close ()
+    print (x)
+    x=x+1
+       
